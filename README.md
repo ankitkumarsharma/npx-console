2e24f7cc23dac97f84044f02f2ca54720ec0e12d
ankitkumarsharma <aksharma.dixit@gmail.com>
2023-10-28 20:55:13 +0530

    add npx console functionality

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # npx-console
 NPX | NPM | Console | Development
+
+## For local run this project
+you have to use >> "npm i -g "
+After run above command , you have to run this command "npx ankit-info"
+
+## Contact Details
+If you want to connect with me, please go with my website, 
+[www.ankitkumarsharma.com](https://www.ankitkumarsharma.com/)
---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+console.log('\x1B[37m=========================================================================');
+console.log('\x1B[31mWelcome Buddy - About me !!! ');
+console.log('\x1B[37m-------------------------------------------------------------------------');
+console.log('\x1B[31m       ████          ████     ██     ██    ██     ██     ██████████\n      ██  ██         ██ ██    ██     ██   ██      ██         ██\n     ██    ██        ██  ██   ██     ██ ███       ██         ██\n    ██  **  ██       ██   ██  ██     ██ ███       ██         ██\n   ██        ██      ██    ██ ██     ██   ██      ██         ██\n  ██          ██     ██     ████     ██    ██     ██         ██');
+console.log('\x1B[37m-------------------------------------------------------------------------');
+console.log('\x1B[32mWebsite       >>>     www.ankitkumarsharma.com ');
+console.log('\x1B[32mLinkedin      >>>     www.linkedin.com/in/ankit-kumar-sharma1101/ ');
+console.log('\x1B[32mMedium        >>>     www.medium.com/@ankit_k_sharma ');
+console.log('\x1B[32mTwitter       >>>     www.twitter.com/ankit_k_sharma ');
+console.log('\x1B[32mGitHub        >>>     www.github.com/ankit-k-sharma ');
+console.log('\x1B[32mNPM           >>>     www.npmjs.com/~ankit_k_sharma ');
+console.log('\x1B[37m=========================================================================');
+
+// For run on local, you have to use >> npm i -g and then npx @ankit/about-me  
