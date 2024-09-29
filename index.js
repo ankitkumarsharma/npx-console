3b63edd1004047a400b9ccd62e0ff0601c749a4e
ankitkumarsharma <aksharma.dixit@gmail.com>
2024-09-30 02:41:42 +0530

    update gitub name

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log('\x1B[32mWebsite       >>>     www.ankitkumarsharma.com ');
 console.log('\x1B[32mLinkedin      >>>     www.linkedin.com/in/ankit-kumar-sharma1101/ ');
 console.log('\x1B[32mMedium        >>>     www.medium.com/@ankit_k_sharma ');
 console.log('\x1B[32mTwitter       >>>     www.twitter.com/ankit_k_sharma ');
-console.log('\x1B[32mGitHub        >>>     www.github.com/ankit-k-sharma ');
+console.log('\x1B[32mGitHub        >>>     www.github.com/ankitkumarsharma ');
 console.log('\x1B[32mNPM           >>>     www.npmjs.com/~ankit_k_sharma ');
 console.log('\x1B[37m=========================================================================');
 
